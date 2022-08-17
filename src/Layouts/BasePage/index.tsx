9b012a1a657d5f255eac1f9b92c18541db1f5738
fadhilrafiii <fadhilrafi90@gmail.com>
2022-08-17 11:55:59 +0700

    Add routes and shared constants + helpers

--- src/Layouts/BasePage/index.tsx
+++ src/Layouts/BasePage/index.tsx
@@ -1,16 +1,40 @@
-import React, { ReactNode } from 'react';
+import React, { ReactNode, useMemo } from 'react';
+
+import { useLocation } from 'react-router-dom';
+import { mainRoutes, secondaryRoutes } from 'Router/routes';
 
 import Sidebar from 'Components/Sidebar';
+
+import PageSubtitle from 'Shared/Constants/PageSubtitle';
+import PageTitle from 'Shared/Constants/PageTitle';
+
+import styles from './index.module.css';
 
 interface BasePageProps {
   children: ReactNode;
 }
 
 const BasePage = ({ children }: BasePageProps) => {
+  const location = useLocation();
+
+  const [pageTitle, pageSubtitle] = useMemo(() => {
+    const path = location.pathname;
+    const key: string = path.split('/').pop() || 'home';
+
+    return [
+      PageTitle[key as keyof typeof PageTitle],
+      PageSubtitle[key as keyof typeof PageSubtitle],
+    ];
+  }, [location]);
+
   return (
-    <div>
-      <Sidebar />
-      <div>{children}</div>
+    <div className={styles.container}>
+      <Sidebar mainRoutes={mainRoutes} secondaryRoutes={secondaryRoutes} />
+      <div className={styles.pageWrapper}>
+        <h3 className={styles.pageTitle}>{pageTitle}</h3>
+        <p className={styles.pageSubtitle}>{pageSubtitle}</p>
+        <div className={styles.contentPage}>{children}</div>
+      </div>
     </div>
   );
 };
