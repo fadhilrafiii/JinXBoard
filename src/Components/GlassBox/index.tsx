b897121e256b6f68f15e64fac5021dc51357fb0c
fadhilrafiii <fadhilrafi90@gmail.com>
2022-08-20 13:33:30 +0700

    Add GlassBox component

--- src/Components/GlassBox/index.tsx
+++ src/Components/GlassBox/index.tsx
@@ -4,17 +4,19 @@ import styles from './index.module.css';
 
 interface GlassBoxProps extends HTMLAttributes<HTMLDivElement> {
   children: ReactNode;
+  hoverClass?: string;
 }
 
-const GlassBox = ({ children, ...props }: GlassBoxProps) => {
-  const { className, style } = props;
+const GlassBox = ({ children, hoverClass, ...props }: GlassBoxProps) => {
+  const { className } = props;
 
   const containerStyles = [styles.container];
 
   if (className) containerStyles.push(className);
+  if (hoverClass) containerStyles.push(hoverClass);
 
   return (
-    <div className={containerStyles.join(' ')} style={styles}>
+    <div {...props} className={containerStyles.join(' ')}>
       {children}
     </div>
   );
